439301951de0fcb69cad444365cbe5fed5dbaeac
Deepak Rattan <deepak.rattan@skycap.co.in>
2020-04-28 13:48:00 +0530

    Final code of QUIZ App

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+// This class is having two fields namely "questionText" for questions and "questionAnswer" for answers .
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  //Constructor
+  Question(String questionText, bool questionAnswer) {
+    this.questionText = questionText;
+    this.questionAnswer = questionAnswer;
+  }
+}
